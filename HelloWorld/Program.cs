adf5512ca046271f4f68ff4d8c16486effef29cc
Bruker1 <Bruker1@DESKTOP-1C2UUC4>
2024-01-08 23:18:44 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    //PascalCase
+    public class Program
+    {
+        //camelCase
+        public static void Main(string[] args)
+        {
+            string firstAndLastName = "Krzysztof";
+
+            //UPPERCASE
+            const double PI_NUMBER = 3.14;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam się Krzysztof Pawłowski");
+            if (PI_NUMBER > 2)
+            {
+                WritePiNumber(PI_NUMBER);
+            }
+        }
+
+        private static string WritePiNumber(double pi)
+        {
+            return ("3.14");
+        }
+
+
+    }
+}
